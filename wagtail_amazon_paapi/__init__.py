bcfc0d86c26a1987393382967e7fdc3de2fd3bb3
Ravi Gupta <ravigupta.art@gmail.com>
2025-06-26 12:24:27 +0530

    models in the package will use BigAutoField without requiring global settings changes

---
+++ wagtail_amazon_paapi/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "wagtail_amazon_paapi.apps.WagtailAmazonPaapiConfig"
---
+++ wagtail_amazon_paapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class WagtailAmazonPaapiConfig(AppConfig):
+    name = "wagtail_amazon_paapi"
+    default_auto_field = "django.db.models.BigAutoField"
